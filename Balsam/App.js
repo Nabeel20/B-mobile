1bbc594b52cbd53344fab8f706f776a9e2e3f84f
Nabeel20 <nabeelandnizam@gmail.com>
2022-01-09 16:02:54 +0200

    Revert "Read operation"
    This reverts commit 59a83a1ad112b79c16be4c857bb4350ccba1a7e8.

--- Balsam/App.js
+++ Balsam/App.js
@@ -340,8 +340,20 @@ export default function App() {
         );
       }
     }
+    async function magic() {
+      let private_files = await FileSystem.ls(Dirs.DocumentDir);
+      let files = [];
+      for (let index = 0; index < private_files.length; index++) {
+        let file = await FileSystem.readFile(private_files[index]);
+        let decoded_file = decode_file(file);
+        let file_content = JSON.parse(decoded_file);
+        files.push(file_content.title);
+      }
+      app_database.debug = files;
+    }
     check_permission();
     read_blsm();
+    magic();
   }, [shouldAskForPermissions, ready]);
   function LoadingScreen() {
     if (shouldAskForPermissions === false) {
