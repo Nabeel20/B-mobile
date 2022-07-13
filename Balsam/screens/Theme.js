37015457a0db6ad46b9289425dd37aa0cfa49268
Nobel <nabeelandnizam@gmail.com>
2022-07-13 07:13:37 +0300

    Update Theme.js

--- Balsam/screens/Theme.js
+++ Balsam/screens/Theme.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import {
+  Text as NativeText,
+  View as ViewNative,
+  TouchableOpacity,
+} from 'react-native';
 const ThemeContext = React.createContext();
 const Colors = {
   green: '#10B981',
@@ -9,6 +14,12 @@ const Colors = {
   blue_light: '#BFDBFE',
 };
 
+const ButtonStyle = {
+  padding: 14,
+  alignItems: 'center',
+  justifyContent: 'center',
+  margin: 4,
+};
 function ThemeProvider({children}) {
   const [darkTheme, setTheme] = React.useState(false);
   const Theme = {
@@ -20,10 +31,83 @@ function ThemeProvider({children}) {
       accent_2: darkTheme ? '#C4C4C4' : '#636363',
     },
   };
+  const Text = function ({
+    style,
+    color = '',
+    secondary,
+    size = 14,
+    padding,
+    textChildren,
+  }) {
+    return (
+      <NativeText
+        style={[
+          {
+            fontFamily: 'Readex pro',
+            color:
+              Colors[color] === undefined
+                ? secondary
+                  ? Theme.grey.accent_2
+                  : Theme.text
+                : Colors[color],
+            fontSize: size,
+            padding: padding === undefined ? null : padding,
+          },
+          style,
+        ]}>
+        {textChildren}
+      </NativeText>
+    );
+  };
+  const Button = function ({
+    onPress,
+    style,
+    color,
+    buttonChildren,
+    round,
+    flex,
+  }) {
+    const background_colors = {
+      blue: Colors.blue_light,
+      red: Colors.red_light,
+    };
+    return (
+      <TouchableOpacity
+        onPress={onPress}
+        style={[
+          {
+            backgroundColor:
+              Colors[color] === undefined
+                ? Theme.grey.default
+                : background_colors[color],
+            borderRadius: round ? 99 : 8,
+            flex: flex === undefined ? null : 1,
+            ...ButtonStyle,
+          },
+          style,
+        ]}>
+        {buttonChildren}
+      </TouchableOpacity>
+    );
+  };
+  const View = function ({background = false, style, viewChildren}) {
+    return (
+      <ViewNative
+        style={[
+          {backgroundColor: background ? Theme.background : null},
+          style,
+        ]}>
+        {viewChildren}
+      </ViewNative>
+    );
+  };
   const value = {
     Theme,
     darkTheme,
     setTheme,
+    Text,
+    Button,
+    View,
   };
 
   return (
