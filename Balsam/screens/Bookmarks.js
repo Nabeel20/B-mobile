02ab942eadb1f45aeb0793206232fa66a80d41ba
Nobel <nabeelandnizam@gmail.com>
2022-07-14 21:52:30 +0300

    Update bookmarks.js
     - clean up
    - rename image file

--- Balsam/screens/Bookmarks.js
+++ Balsam/screens/Bookmarks.js
@@ -1,167 +1,137 @@
 import React from 'react';
-import {
-  View,
-  StyleSheet,
-  Text,
-  FlatList,
-  TouchableOpacity,
-  Modal,
-  ScrollView,
-  Image,
-} from 'react-native';
+import {StyleSheet, FlatList, Modal, ScrollView, Image} from 'react-native';
 import BackButton from './components/Back.button';
 import {Colors, ThemeContext} from './Theme';
 
-function Question({data, onPress, theme}) {
-  return (
-    <TouchableOpacity
-      onPress={onPress}
-      style={[
-        styles.listContentContainer,
-        {
-          backgroundColor: theme.grey.default,
-        },
-      ]}>
-      <View style={styles.subjectContainer}>
-        <Image
-          source={require('../assets/book.icon.png')}
-          style={styles.image}
-        />
-        <Text style={styles.subject}>{data.subject}</Text>
-      </View>
-      <Text style={[styles.text, {color: theme.text}]}>{data.question}</Text>
-    </TouchableOpacity>
-  );
-}
-function EmptyList({Theme}) {
-  return (
-    <View style={styles.emptyListContainer}>
-      <Image
-        source={require('../assets/empty.icon.png')}
-        style={styles.emptyImage}
-      />
-      <Text style={[styles.text, {color: Theme.text}]}>
-        لم تحفظ أي من الأسئلة
-      </Text>
-    </View>
-  );
-}
-
-export default function Bookmarks({navigation, route, data}) {
+export default function Bookmarks({navigation, route, storage}) {
+  const {subject} = route.params;
+  const [bookmarksData, setBookmarksData] = React.useState([]);
   const [modalVisible, setModalVisible] = React.useState(false);
-  const [dataIndex, setDataIndex] = React.useState(0);
-  const {Theme} = React.useContext(ThemeContext);
-  function open_question_modal(i) {
-    setDataIndex(i);
+  const [selected_question, set_selected_question] = React.useState({});
+  const {Theme, Button, Text, View} = React.useContext(ThemeContext);
+  React.useEffect(() => {
+    if (storage === undefined) {
+      return;
+    }
+    let bookmarks_on_disk = JSON.parse(storage.getString('bookmarks'));
+    if (subject !== '') {
+      bookmarks_on_disk = bookmarks_on_disk.filter(
+        item => item.subject === subject,
+      );
+    }
+    setBookmarksData(bookmarks_on_disk);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  function open_question_modal(question_details) {
+    question_details.choices = question_details.choices.filter(
+      item => item !== question_details.correct_answer,
+    );
+    question_details.has_explanation = question_details.explanation.length > 5;
+    set_selected_question(question_details);
     setModalVisible(true);
     return;
   }
-  const {subject} = route.params;
-  if (subject !== '') {
-    data = data.filter(item => item.subject === subject);
+
+  const Question = function ({item}) {
+    return (
+      <Button
+        key={item.id}
+        onPress={() => open_question_modal(item)}
+        style={styles.list_card}>
+        <Text weight="medium" size={16}>
+          {item.question}
+        </Text>
+        <View style={styles.list_tags_container}>
+          <Tag>{item.subject}</Tag>
+          <Tag>{item.title}</Tag>
+        </View>
+      </Button>
+    );
+  };
+  const EmptyList = function () {
+    return (
+      <View style={styles.empty_list_container}>
+        <Image
+          source={require('../assets/empty-bookmarks-icon.png')}
+          style={styles.empty_image}
+        />
+        <Text style={styles.empty_list_text}>لم تحفظ أي من الأسئلة</Text>
+      </View>
+    );
+  };
+  function Tag({children}) {
+    return (
+      <View style={[styles.tag, {backgroundColor: Theme.grey.default}]}>
+        <Text size={12}>{children}</Text>
+      </View>
+    );
   }
+
   return (
-    <View style={[styles.container, {backgroundColor: Theme.background}]}>
+    <View style={styles.container} background>
       <Modal
         animationType="slide"
         visible={modalVisible}
         onRequestClose={() => {
           setModalVisible(!modalVisible);
         }}>
-        {data.length !== 0 ? (
-          <View style={[styles.container, {backgroundColor: Theme.background}]}>
-            <View style={styles.header}>
-              <BackButton onPress={() => setModalVisible(false)} />
-              <TouchableOpacity
-                onPress={() => setModalVisible(false)}
-                style={styles.deleteButton}>
-                <Text style={[styles.helperText, {color: Theme.text}]}>
-                  إزالة من المحفوظات
-                </Text>
-              </TouchableOpacity>
-            </View>
-            <View style={{height: '25%'}} />
-            <View style={styles.questionContainer}>
-              <Image
-                source={require('../assets/info.icon.png')}
-                style={[
-                  styles.infoIcon,
-                  {
-                    tintColor: Colors.blue,
-                  },
-                ]}
-              />
-              <Text
-                style={[
-                  styles.question,
-                  {color: Theme.text, marginBottom: 32},
-                ]}>
-                {data[dataIndex].question}
-              </Text>
-            </View>
-
-            <ScrollView contentContainerStyle={styles.modalScroll}>
-              <View
-                style={[styles.card, {backgroundColor: Colors.green_light}]}>
-                <Text style={[styles.text, {color: Colors.green}]}>
-                  الإجابة الصحيحة
-                </Text>
-                <Text style={[styles.text, {color: Theme.text}]}>
-                  {data[dataIndex].right_answer}
-                </Text>
-              </View>
-
-              <View
-                style={[styles.card, {backgroundColor: Theme.grey.default}]}>
-                <Text style={[styles.text, {color: Theme.grey.accent_2}]}>
-                  باقي الخيارات
-                </Text>
-                {data[dataIndex].choices
-                  .filter(c => c !== data[dataIndex].right_answer)
-                  .map((choice, index) => {
-                    return (
-                      <Text
-                        key={index}
-                        style={[styles.text, {color: Theme.text}]}>
-                        • {choice}
-                      </Text>
-                    );
-                  })}
-              </View>
-              {data[dataIndex].explanation.length > 3 ? (
-                <View
-                  style={[styles.card, {backgroundColor: Theme.grey.default}]}>
-                  <Text style={[styles.text, {color: Theme.grey.accent_2}]}>
-                    شرح السؤال
-                  </Text>
-                  <Text style={styles.text}>{data[dataIndex].explanation}</Text>
-                </View>
-              ) : null}
-            </ScrollView>
+        <View style={styles.container} background>
+          <View style={styles.header}>
+            <BackButton onPress={() => setModalVisible(false)} />
+            <Button
+              onPress={() => setModalVisible(false)}
+              color="red"
+              style={styles.remove_button}>
+              <Text style={styles.remove_button_text}>إزالة من المحفوظات</Text>
+            </Button>
           </View>
-        ) : null}
+          <View style={styles.tags_container}>
+            <Tag>{selected_question.subject}</Tag>
+            <Tag>{selected_question.title}</Tag>
+          </View>
+          <Text style={styles.question} weight="medium">
+            {selected_question.question}
+          </Text>
+
+          <ScrollView contentContainerStyle={styles.modalScroll}>
+            <Text secondary style={styles.modal_h2}>
+              الإجابة الصحيحة
+            </Text>
+            <View style={styles.correct_answer_container}>
+              <Text>{selected_question.correct_answer}</Text>
+              <View style={styles.green_dot} />
+            </View>
+            <Text secondary padding={8}>
+              باقي الخيارات
+            </Text>
+            {selected_question.choices?.map((choice, index) => (
+              <Text padding={4} key={index}>
+                {' '}
+                - {choice}
+              </Text>
+            ))}
+            {selected_question.has_explanation ? (
+              <>
+                <Text secondary style={styles.modal_h2}>
+                  التوضيح
+                </Text>
+                <Text>{selected_question.explanation}</Text>
+              </>
+            ) : null}
+          </ScrollView>
+        </View>
       </Modal>
 
       <BackButton onPress={() => navigation.goBack()} />
-      <View style={{height: '20%'}} />
-
-      <Text style={[styles.title, {color: Theme.text}]}>
+      <Text style={styles.title} weight="bold">
         {`المحفوظات${subject !== '' ? ': ' + subject : ''}`}
-        <Text style={[styles.helperText, {color: Theme.grey.accent_2}]}>
-          {'\n'}الأسئلة التي حفظتها
-        </Text>
       </Text>
       <FlatList
-        data={data}
-        ListEmptyComponent={() => <EmptyList Theme={Theme} />}
+        contentContainerStyle={styles.empty_list_container}
+        data={bookmarksData}
+        ListEmptyComponent={EmptyList}
         keyExtractor={item => item.id}
-        renderItem={({item, index}) => (
-          <Question
-            data={item}
-            theme={Theme}
-            onPress={() => open_question_modal(index)}
-          />
-        )}
+        renderItem={Question}
       />
     </View>
   );
@@ -172,88 +142,77 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 16,
   },
+  list_tags_container: {
+    flexDirection: 'row',
+    marginTop: 8,
+  },
+  tags_container: {
+    flexDirection: 'row',
+    marginBottom: 4,
+    justifyContent: 'flex-end',
+    marginTop: '10%',
+  },
   title: {
-    fontFamily: 'ReadexPro-Bold',
     fontSize: 20,
     marginRight: 16,
     marginBottom: 16,
+    marginTop: '20%',
   },
-  listContentContainer: {
-    borderRadius: 10,
-    padding: 16,
+  list_card: {
     margin: 8,
+    alignItems: 'flex-end',
   },
-  helperText: {
-    fontFamily: 'ReadexPro-Regular',
-    fontSize: 14,
-  },
-  subject: {
-    fontFamily: 'ReadexPro-Bold',
-    fontSize: 14,
-    color: Colors.blue,
-  },
-  text: {
-    fontFamily: 'ReadexPro-Medium',
+  empty_list_text: {
     fontSize: 16,
     padding: 4,
   },
-  modalView: {
-    flex: 1,
-    padding: 8,
-  },
-  card: {
-    padding: 14,
-    borderRadius: 10,
-    marginBottom: 16,
-  },
-  subjectContainer: {
-    flexDirection: 'row-reverse',
-    alignSelf: 'flex-end',
+  tag: {
     padding: 6,
-  },
-  image: {
-    width: 16,
-    height: 16,
-    tintColor: Colors.blue,
+    borderRadius: 99,
     marginHorizontal: 4,
   },
   modalScroll: {
     padding: 8,
   },
-  questionContainer: {
-    flexDirection: 'row-reverse',
-    alignItems: 'baseline',
-    padding: 8,
-  },
   question: {
-    fontFamily: 'ReadexPro-Medium',
-    fontSize: 20,
-    marginBottom: 32,
+    fontSize: 18,
+    margin: 4,
   },
   header: {
     flexDirection: 'row-reverse',
     justifyContent: 'space-between',
     alignItems: 'center',
   },
-  deleteButton: {
-    backgroundColor: Colors.red_light,
-    height: 40,
+  remove_button: {
     borderRadius: 8,
     padding: 8,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  emptyImage: {
+  remove_button_text: {
+    color: '#141414',
+  },
+  empty_image: {
     width: 80,
     height: 80,
   },
-  emptyListContainer: {
+  empty_list_container: {
+    flex: 1,
     alignItems: 'center',
-    marginTop: 80,
+    justifyContent: 'center',
   },
-  infoIcon: {
-    height: 24,
-    width: 24,
+  modal_h2: {
+    fontSize: 16,
+    padding: 8,
+  },
+  correct_answer_container: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
     padding: 4,
+  },
+  green_dot: {
+    height: 10,
+    width: 10,
+    backgroundColor: Colors.green,
+    borderRadius: 5,
+    margin: 4,
   },
 });
