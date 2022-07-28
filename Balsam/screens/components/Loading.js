ce18281bf017c5e92010c0e287269430737c041f
Nobel <nabeelandnizam@gmail.com>
2022-07-28 13:12:10 +0300

    Update Loading.js

--- Balsam/screens/components/Loading.js
+++ Balsam/screens/components/Loading.js
@@ -1,68 +1,41 @@
 import React from 'react';
-import {
-  View,
-  StyleSheet,
-  Text,
-  Image,
-  ActivityIndicator,
-  TouchableOpacity,
-} from 'react-native';
-import {ThemeContext, Colors} from '../Theme';
+import {StyleSheet, Image, ActivityIndicator} from 'react-native';
+import {ThemeContext} from '../Theme';
+import {useNetInfo} from '@react-native-community/netinfo';
 
-export default function Loading({status = true, onPress}) {
-  const {Theme} = React.useContext(ThemeContext);
+export default function Loading({
+  status = true,
+  onPress,
+  text = 'حاول مرة أخرى',
+}) {
+  const netInfo = useNetInfo() ?? {isConnected: true};
+  const {Theme, View, Button, Text} = React.useContext(ThemeContext);
+  if (netInfo.isConnected === false) {
+    status = false;
+  }
+  let message = status ? 'جار التحميل...' : 'للأسف حدث خطأ بتحميل البيانات';
 
   return (
-    <View
-      style={[
-        styles.container,
-        {
-          backgroundColor: Theme.background,
-        },
-      ]}>
-      <View style={{alignSelf: 'center'}}>
-        {status ? (
-          <View>
-            <Image
-              source={require('../../assets/logo-icon.png')}
-              style={styles.image}
-            />
-            <Text
-              style={[
-                styles.title,
-                {
-                  color: Theme.text,
-                },
-              ]}>
-              {' '}
-              جار التحميل{' '}
-            </Text>
-            <ActivityIndicator
-              size="small"
-              color={Theme.text}
-              style={{alignSelf: 'center'}}
-            />
-          </View>
-        ) : (
-          <View>
-            <Image
-              source={require('../../assets/error.icon.png')}
-              style={styles.image}
-            />
-            <Text
-              style={[
-                styles.title,
-                {
-                  color: Colors.red,
-                },
-              ]}>
-              فشل تحميل البيانات
-            </Text>
+    <View style={styles.container} background>
+      <View style={styles.centred_box}>
+        <Image
+          source={
+            status
+              ? require('../../assets/happy-icon.png')
+              : require('../../assets/error-icon.png')
+          }
+          style={styles.image}
+        />
+        <Text style={styles.title} weight="medium">
+          {netInfo.isConnected === false ? 'لست متصلاً بالانترنت' : message}
+        </Text>
 
-            <TouchableOpacity onPress={onPress} style={styles.button}>
-              <Text style={styles.text}> حاول مرة أخرى</Text>
-            </TouchableOpacity>
-          </View>
+        {status === false ? (
+          <Button round onPress={onPress}>
+            <Text weight="medium">{text}</Text>
+          </Button>
+        ) : (
+          <ActivityIndicator size="small" color={Theme.text} />
         )}
       </View>
     </View>
@@ -73,30 +46,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
-    height: '100%',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  centred_box: {
+    alignSelf: 'center',
+  },
   title: {
-    fontFamily: 'ReadexPro-Bold',
-    fontSize: 21,
+    fontSize: 18,
     margin: 16,
-  },
-  text: {
-    fontFamily: 'ReadexPro-Medium',
-    fontSize: 16,
-    color: Colors.blue,
-  },
-  button: {
-    backgroundColor: Colors.blue_light,
-    borderRadius: 8,
-    padding: 8,
-    alignItems: 'center',
-    marginTop: 32,
+    textAlign: 'center',
   },
   image: {
-    width: 104,
-    height: 104,
+    width: 80,
+    height: 80,
     alignSelf: 'center',
   },
 });
